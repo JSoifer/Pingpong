0f66ed9b7aad9abd902a34544b0c787c54534e6a
Julianne Soifer <juliannesoifer@gmail.com>
2018-06-23 16:26:01 -0700

    begin to add and test business logic. not working so far.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
+var pingpong =function(number1) {
+  if (number1 % 3===0);
+  alert("Ping");
+}
+
+
+
+
+
 $(document).ready(function() {
   $("form#ping-Pong").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("input#number").val());
-    var result = function(number1);
+  //  var result = function(number1);
     $("#result").text(result);
   });
 });
