8cd77157f1b92d66e59965427a6ba73799753f62
Julianne Soifer <juliannesoifer@gmail.com>
2018-06-24 16:16:34 -0700

    add more specific business logic, continue to try and get the output to display. still failing miserably.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ var pingPong = (function(number1) {
   var numberList = []
 
     for ( var i = 1; i <= number1; i ++);{
-      if (i % 3 === 0) {
+      if (i % 3 === 0 && i % 5 !== 0) {
         numberList.push("ping");
       }
-      else if (i % 5 === 0) {
+      else if (i % 5 === 0 && i % 3 !== 0) {
         numberList.push("pong");
       }
       else if (i % 3 === 0 && i % 5 === 0 ) {
@@ -14,8 +14,8 @@ var pingPong = (function(number1) {
       else {
         numberList.push(i);
       }
-    return numberList;
   };
+  return numberList;
 
 
 });
@@ -26,19 +26,21 @@ var pingPong = (function(number1) {
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
+
     var number1 = parseInt($("input#number").val());
+
     if (isNaN(number1)) {
       alert("Enter a number");
     }
-    var returnedNumberList = pingPong(number1);
-
     $("#result").empty();
+
+    var returnedNumberList = pingPong(number1);
     returnedNumberList.forEach(function(listItem) {
-     $("<li>" + listItem + "</li>").appendTo("#result");
-});
+     $("<li>" + listItem + "</li>").append("#result");
+   });
     //$("#result").show();
-  //pingpong(numberList);
-  event.preventDefault();
+    //pingPong(numberList);
+    event.preventDefault();
 
   });
 });
