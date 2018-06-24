50481d3ea43c131c82c3a6ece5815bac49526671
Julianne Soifer <juliannesoifer@gmail.com>
2018-06-24 14:00:04 -0700

    add ui login to get results to display. results is displaying but it is incorrect.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 var pingPong = (function(number1) {
   var numberList = []
 
-    for ( var index = 1; index <= number1; index ++);{
-  //} else {
-    numberList.push(index);
-
+    for ( var i = 1; i <= number1; i ++);{
+      if (i % 3 === 0) {
+        numberList.push("ping");
+      }
+      else if (i % 5 === 0) {
+        numberList.push("pong");
+      }
+      else if (i % 3 === 0 && i % 5 === 0 ) {
+        numberList.push("pingpong");
+      }
+      else {
+        numberList.push(i);
+      }
+    return numberList;
   };
-
-    //numberList.push("ping");
 
 
 });
@@ -22,9 +30,14 @@ $(document).ready(function() {
     if (isNaN(number1)) {
       alert("Enter a number");
     }
-    var returnedArray = pingPong(number1);
-    $("#result").show();
-  pingpong(numberList);
+    var returnedNumberList = pingPong(number1);
+
+    $("#result").empty();
+    returnedNumberList.forEach(function(listItem) {
+     $("<li>" + listItem + "</li>").appendTo("#result");
+});
+    //$("#result").show();
+  //pingpong(numberList);
   event.preventDefault();
 
   });
