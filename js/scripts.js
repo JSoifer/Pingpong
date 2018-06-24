e8eb932f057876c42414c2a93e038a1fbac2a024
Julianne Soifer <juliannesoifer@gmail.com>
2018-06-23 17:26:13 -0700

    struggle with script testing. not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-var pingpong =function(number1) {
-  if (number1 % 3===0);
-  alert("Ping");
-}
+var pingpong = (function(number1) {
+  if (isNAN(number1)) {
+    alert("Enter a number");
+  } else if (number1 % 3 === 0);
+    alert("Ping");
+});
 
 
 
@@ -11,7 +13,8 @@ $(document).ready(function() {
   $("form#ping-Pong").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("input#number").val());
+    var numberList = []
   //  var result = function(number1);
-    $("#result").text(result);
+    $("#result").show(result);
   });
 });
